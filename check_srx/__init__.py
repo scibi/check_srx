41274079323a707b2484c570d4b0fddedb982b26
Patryk Ściborek <patryk@sciborek.com>
2015-07-23 23:57:56 +0200

    Added ChassisClusterStatus table

--- check_srx/__init__.py
+++ check_srx/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 """check-srx - Icinga/Nagios plugin for Juniper SRX firewalls"""
 
 __version__ = '0.1.0'
---
+++ check_srx/tables/chassis_cluster_status.py
@@ -0,0 +1,8 @@
+"""
+Pythonifier chassis cluster status Table/View
+"""
+from jnpr.junos.factory import loadyaml
+from os.path import splitext
+_YAML_ = splitext(__file__)[0] + '.yml'
+globals().update(loadyaml(_YAML_))
+
